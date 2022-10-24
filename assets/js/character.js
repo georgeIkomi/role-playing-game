49afc98af5cd5e35d8fc48593c21e65349c7dfca
Eyituoyo George Ikomi <ikomieyituoyogeorge@yahoo.com>
2022-10-24 04:23:49 +0000

    Create method to simulate damage on character

--- assets/js/character.js
+++ assets/js/character.js
@@ -49,4 +49,38 @@ class Character {
             .map((num) => `<div class="dice">${num}</div>`)
             .join("");
     }
+
+    /**
+     * The objective of this method is to simulate the damage 
+     * inflicted on each character, and this method takes one 
+     * parameter (attackScoreArray) representing the 
+     * "currentDiceScore" of the character. The in-built JavaScript 
+     * .reduce() method is used to calculate and return the total 
+     * score (represented by the total of every element/number in 
+     * the array) forthe character as it iterates over each element 
+     * in the "attackScore" array. This method takes two arguments:
+     * the first is a callback function and the second is the initial 
+     * value of the operation being performed (in this case 0 as it is 
+     * a calculation operation). The callback function to the .reduce()
+     * method also takes two parameters (the first is a variable or
+     * accumulator that is returned by the .reduce() method, and the
+     * second is the current element being iterated over). The total 
+     * score is then stored in the variable (or accumulator) "total"
+     * and is returned and saved in the variable "totalAttackScore".
+     * The "health" value for the character is then reduced by an 
+     * amount equal to the "totalAttackScore". Finally, a 
+     * determination is made as to whether the character is dead,
+     * and in so doing ensures that the health value of the 
+     * character never assumes a negative value.
+     */
+    takeDamage(attackScoreArray) {
+        const totalAttackScore = attackScoreArray
+            .reduce((total, num) => total += num,0);
+
+        this.health -= totalAttackScore;
+        if (this.health <= 0) {
+            this.health = 0;
+            this.dead = true;
+        }
+    }
 }
