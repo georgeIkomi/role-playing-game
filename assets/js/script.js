a38dda08d0c03c80474deae22b55ccf746ffafe1
Eyituoyo George Ikomi <ikomieyituoyogeorge@yahoo.com>
2022-10-25 05:23:46 +0000

    Create function to provide new monster as game unfolds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 import characterData from "./data.js";
 import Character from "./character.js";
 import { newMonstersArray } from "./utils.js";
+
+const monstersArray = newMonstersArray();
+let isWaiting = false;
+
+/**
+ * This function takes the first monster from "monstersArray"
+ * (using the in-build JavaScript method ".shift()")
+ * and extracts that monsters data from the data.js file, saves 
+ * it in the variable "nextMonsterData", and returns a new 
+ * instance of the extracted monster's Character. The function 
+ * returns an empty object {} if there are no more monsters in 
+ * "monstersArray".
+ */
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()];
+    return nextMonsterData ? new Character[nextMonsterData]: {};
+}
