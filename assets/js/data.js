7304380225558ab097a0407c6f26070df58241c4
Eyituoyo George Ikomi <ikomieyituoyogeorge@yahoo.com>
2022-10-26 00:12:43 +0000

    Add correct file path for images

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,28 +5,28 @@
 const characterData = {
     hero: {
       name: "Wizard",
-      avatar: "./images/wizard.png",
+      avatar: "../assets/images/wizard.png",
       health: 60,
       diceCount: 3,
       currentDiceScore: [],
     },
     orc: {
       name: "Orc",
-      avatar: "./images/orc.png",
+      avatar: "../assets/images/orc.png",
       health: 30,
       diceCount: 1,
       currentDiceScore: [],
     },
     demon: {
       name: "Demon",
-      avatar: "./images/demon.png",
+      avatar: "../assets/images/demon.png",
       health: 25,
       diceCount: 2,
       currentDiceScore: [],
     },
     goblin: {
       name: "Goblin",
-      avatar: "./images/goblin.png",
+      avatar: "../assets/images/goblin.png",
       health: 20,
       diceCount: 3,
       currentDiceScore: [],
